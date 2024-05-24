c0ad480c6655c6026ddc1073e7cea68d0151a51c
Енушкевич Всеволод Владимирович <vvenushkevich@edu.hse.ru>
2024-05-25 02:25:16 +0300

    connection between two part of code

--- core/parsingfunc.py
+++ core/parsingfunc.py
@@ -1,13 +1,12 @@
 import pandas as pd
 import requests
-import schedule
-import time
 import asyncio
-from core.main import anomaly_report
 from aiogram import Bot
 from core.settings import settings
-import analyzer
-file_path = 'Russian_Stocks_on_MOEX.csv'
+from core.main import anomaly_report
+
+file_path = '../Russian_Stocks_on_MOEX.csv'
+
 try:
     df = pd.read_csv(file_path, sep=';', on_bad_lines='skip')
 except FileNotFoundError:
@@ -16,13 +15,16 @@ except FileNotFoundError:
 except pd.errors.ParserError as e:
     print(f"Ошибка парсинга: {e}")
     exit()
+
 closing_prices = {}
 company_names = {}
+
 for index, row in df.iterrows():
     ticker = row['Ticker']
     closing_prices[ticker] = []
     company_names[ticker] = row['Company']
-def get_stock_data(symbol, api_key):
+
+async def get_stock_data(symbol, api_key):
     url = 'https://www.alphavantage.co/query'
     params = {'function': 'TIME_SERIES_INTRADAY','symbol': symbol,'interval': '5min','apikey': api_key,'outputsize': 'compact'}
     response = requests.get(url, params=params)
@@ -41,21 +43,21 @@ def get_stock_data(symbol, api_key):
             print(f"Нет данных во временном ряде для {symbol}.")
     else:
         print(f"Ошибка {response.status_code} при запросе данных для {symbol}.")
-def process_all_stocks():
+
+async def process_all_stocks():
     for index, row in df.iterrows():
         symbol = row['Ticker']
         api_key = row['key']
-        get_stock_data(symbol, api_key)
+        await get_stock_data(symbol, api_key)
     analysis_data = {company_names[ticker]: prices for ticker, prices in closing_prices.items()}
     print("Последние данные о закрывающих ценах акций:")
     return analysis_data
-    #analyzer.analyze_data(analysis_data)
-schedule.every().hour.do(process_all_stocks)
-while True:
-    schedule.run_pending()
-    time.sleep(1)
+
 async def calculate_percent(name, values):
-    condition = False
+    if len(values) < 2:
+        print(f"Недостаточно данных для символа {name}.")
+        return False, 0, name
+
     prev_price = values[0]
     now_price = values[1]
     change_percent = ((now_price - prev_price) / prev_price) * 100
@@ -65,11 +67,13 @@ async def calculate_percent(name, values):
 
 async def main():
     bot = Bot(token=settings.bots.bot_token)
-    analysis_data = process_all_stocks()
-    for name, values in analysis_data.items():
-        condition, percent, newname = await calculate_percent(name, values)
-        if condition:
-            await anomaly_report(bot, newname, percent)
+    while True:
+        analysis_data = await process_all_stocks()
+        for name, values in analysis_data.items():
+            condition, percent, newname = await calculate_percent(name, values)
+            if condition:
+                await anomaly_report(bot, newname, percent)
+        await asyncio.sleep(3600)
 
 if __name__ == '__main__':
     asyncio.run(main())
