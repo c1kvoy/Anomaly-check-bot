371b3cc91fa6ebb91e5bd1adcdf15d369a9c20bc
Енушкевич Всеволод Владимирович <vvenushkevich@edu.hse.ru>
2024-06-07 01:35:55 +0300

    добавлена база данных, изменены кнопки, дизайн бота

--- core/parsingfunc.py
+++ core/parsingfunc.py
@@ -1,65 +1,14 @@
-import pandas as pd
-import requests
 import asyncio
+from main import anomaly_report
 from aiogram import Bot
 from core.settings import settings
-from core.main import anomaly_report
 
-file_path = '../Russian_Stocks_on_MOEX.csv'
-
-try:
-    df = pd.read_csv(file_path, sep=';', on_bad_lines='skip')
-except FileNotFoundError:
-    print(f"Файл не найден по указанному пути: {file_path}")
-    exit()
-except pd.errors.ParserError as e:
-    print(f"Ошибка парсинга: {e}")
-    exit()
-
-closing_prices = {}
-company_names = {}
-
-for index, row in df.iterrows():
-    ticker = row['Ticker']
-    closing_prices[ticker] = []
-    company_names[ticker] = row['Company']
-
-async def get_stock_data(symbol, api_key):
-    url = 'https://www.alphavantage.co/query'
-    params = {'function': 'TIME_SERIES_INTRADAY','symbol': symbol,'interval': '5min','apikey': api_key,'outputsize': 'compact'}
-    response = requests.get(url, params=params)
-    if response.status_code == 200:
-        data = response.json()
-        time_series = data.get('Time Series (5min)', {})
-        if time_series:
-            last_time = sorted(time_series.keys())[-1]
-            last_data = time_series[last_time]
-            close_price = float(last_data['4. close'])
-            if symbol in closing_prices:
-                closing_prices[symbol].append(close_price)
-                if len(closing_prices[symbol]) > 2:
-                    closing_prices[symbol].pop(0)
-        else:
-            print(f"Нет данных во временном ряде для {symbol}.")
-    else:
-        print(f"Ошибка {response.status_code} при запросе данных для {symbol}.")
-
-async def process_all_stocks():
-    for index, row in df.iterrows():
-        symbol = row['Ticker']
-        api_key = row['key']
-        await get_stock_data(symbol, api_key)
-    analysis_data = {company_names[ticker]: prices for ticker, prices in closing_prices.items()}
-    print("Последние данные о закрывающих ценах акций:")
-    return analysis_data
-
-async def calculate_percent(name, values):
-    if len(values) < 2:
-        print(f"Недостаточно данных для символа {name}.")
-        return False, 0, name
-
-    prev_price = values[0]
-    now_price = values[1]
+data = [14,20]
+name = 'Gazprom'
+def calculate_percent(data, name):
+    condition = False
+    prev_price = data[0]
+    now_price = data[1]
     change_percent = ((now_price - prev_price) / prev_price) * 100
     if abs(change_percent) > 1:
         condition = True
@@ -67,13 +16,9 @@ async def calculate_percent(name, values):
 
 async def main():
     bot = Bot(token=settings.bots.bot_token)
-    while True:
-        analysis_data = await process_all_stocks()
-        for name, values in analysis_data.items():
-            condition, percent, newname = await calculate_percent(name, values)
-            if condition:
-                await anomaly_report(bot, newname, percent)
-        await asyncio.sleep(3600)
+    condition, percent, newname = calculate_percent(data, name)
+    if condition:
+        await anomaly_report(bot, newname, percent)
 
 if __name__ == '__main__':
     asyncio.run(main())
